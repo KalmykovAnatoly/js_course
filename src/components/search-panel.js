1174468e59b646361a8447d0b8c6b93222d67c21
Kalmykov <kalmikovanatoliy@mail.ru>
2019-01-19 21:23:00 +0300

    - Fixed structure of project

--- src/components/search-panel.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SearchPanel = () => {
-    const searchText = "Type here to search";
-    const searchStyle = {fontSize: '25px'};
-    return (<input placeholder={searchText} style={searchStyle}/>)
-};
-
-export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-
-import AppHeader from './components/app-header'
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-
-const App = () => {
-
-    const isLoggedIn = true;
-    const loginBox = <span>Log in please</span>;
-    const welcomeBox = <span>Welcome</span>;
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome App', important: true, id: 2},
-        {label: 'Have a Lunch', important: false, id: 3}
-    ];
-
-    return (
-        <div>
-            {isLoggedIn ? welcomeBox : loginBox}
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    )
-};
-
+import App from "./components/app/app";
+import React from "react";
+import './index.css'
 
 ReactDOM.render(<App/>, document.getElementById("root"));
